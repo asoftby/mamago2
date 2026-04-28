94efe4e854ef8ac650c2babfe53be640acef4b57
Aliaksei Shapovalov <asoftby@gmail.com>
2026-04-28 16:03:11 +0300

    refactor: update me layout

--- src/app/(public)/me/layout.tsx
+++ src/app/(public)/me/layout.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 /**
- * Личный кабинет /me: тот же UI-шрифт, что и на публичном сайте (NT Somic + стек из `--font-sans`).
+ * Личный кабинет /me: шрифт наследуется от root layout через <html className={ntSomic.variable}>.
  */
 export default function MeLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-w-0 font-nt-somic antialiased">{children}</div>;
+  return <div className="min-w-0 antialiased">{children}</div>;
 }
