b3289d20fcb3a6e9e991e3f71f7c584cbfd066e1
Aliaksei Shapovalov <asoftby@gmail.com>
2026-07-07 00:54:48 +0300

    feat(migration): add media import writer

---
+++ src/lib/migration/media/index.ts
@@ -0,0 +1,11 @@
+export { MediaImportWriter } from "./MediaImportWriter";
+export type {
+  ImportedMediaResult,
+  ImportMediaFromUrlInput,
+  ImportWordPressAttachmentInput,
+  ImportWordPressAttachmentResult,
+  MediaImporterLike,
+  MediaLineageWriterLike,
+} from "./types";
+export { createMamagoMediaImporter } from "./mamagoMediaImporter";
+export type { CreateMamagoMediaImporterDeps } from "./mamagoMediaImporter";
